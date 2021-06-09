4a66b776f262dc215f8a8a0921b468ad1e7d6aba
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-06-09 17:23:51 -0500

    began assignment

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    // put DOM code here to ensure elements have been loaded
+    console.log('window loaded');
+
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        console.log(response);
+    });
+});
