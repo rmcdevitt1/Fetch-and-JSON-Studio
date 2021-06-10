23bc25bf376eb78e1e4bf1be0a774a49fcd53bf1
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-06-09 20:08:16 -0500

    finished

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 // TODO: add code here
-window.addEventListener("load", function() {
-    // put DOM code here to ensure elements have been loaded
-    console.log('window loaded');
+// window.addEventListener("load", function() {
+
+//     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+         
+//     });
+// });
 
 
+
+
+window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
-        console.log(response);
+        response.json().then(function(json) {
+            let div = document.getElementById("container");
+            for (i = 0; i < json.length; i++) {
+                div.innerHTML += `
+                    <div class = "astronaut">
+                        <div class = "bio">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                                <ul>
+                                    <li>Hours in Space: ${json[i].hoursInSpace}</li>
+                                    <li>Active: ${json[i].active}</li>
+                                    <li>Skills: ${json[i].skills.join(", ")}</li>
+                                </ul>
+                            </div>
+                            <img class="avatar" src="${json[i].picture}">
+                        </div>
+                    `;
+            }
+        });
     });
 });
